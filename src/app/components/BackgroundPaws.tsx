830a165294d47907a0445b0ad4cf7ccc0d47e772
geesunny <jiseonyang08@gmail.com>
2025-10-02 17:52:51 +0900

    feat: 배경 패턴 적용 및 BackgroundPaws 컴포넌트 추가

---
+++ src/app/components/BackgroundPaws.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function BackgroundPaws({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="relative min-h-screen w-full p-6 flex justify-center bg-paws-gradient">
+            <div aria-hidden className="pointer-events-none absolute inset-0 bg-paws-pattern opacity-[0.06]" />
+
+            <div className="relative z-10">{children}</div>
+        </div>
+    );
+}
